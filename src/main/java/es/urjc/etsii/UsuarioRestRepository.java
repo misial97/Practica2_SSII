efc9bc76b050147872ad3f73f9af95d6a90ba687
misial97 <35803878+misial97@users.noreply.github.com>
2019-03-24 16:17:49 +0100

    Añadida variable global idSer para el tema de modificar si o si el servicio que se ha buscado

--- src/main/java/es/urjc/etsii/UsuarioRestRepository.java
+++ src/main/java/es/urjc/etsii/UsuarioRestRepository.java
@@ -1,7 +1,11 @@
 package es.urjc.etsii;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 /**
  * Repositorio generador de API REST HATEOAS: servicios
@@ -11,5 +15,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuarios")
 public interface UsuarioRestRepository extends CrudRepository<Usuario, Integer> {
+    @Query(value = "SELECT * FROM USUARIO", nativeQuery = true)
+    List<Usuario> todos_usuarios();
+
+    List<Usuario> findByEmail(@Param("email")String email);
 
 }
